40ecfcef0f07077d5282cd49e80c03ceacc09cd1
betsig <b.signal@garvan.org.au>
2017-02-15 14:39:39 +1100

    changed exons to a GRanges object and removed ability to write file

--- R/gtfToExons.R
+++ R/gtfToExons.R
@@ -2,7 +2,6 @@
 #'
 #' Converts a GTF annotation to exon locations
 #' @param gtf file containing the gtf annotation.
-#' @param outputFile (optional) name of the file to write the exon annotation to.
 #' @return exon annotation data.frame
 #' @export
 #' @import stringr
@@ -17,7 +16,7 @@
 #' exons <- gtfToExons(small_gtf)
 #' @author Beth Signal
 
-gtfToExons <- function(gtf, outputFile=NA){
+gtfToExons <- function(gtf){
 
   gtf.rtrack <- rtracklayer::import(gtf)
 
@@ -33,12 +32,12 @@ gtfToExons <- function(gtf, outputFile=NA){
 
   if(all(!grepl("exon", names(gtf.rtrack@elementMetadata)))){
     gtf.rtrack@elementMetadata$exon_number <- NA
-    gtf.rtrack@elementMetadata$exon_id <- gtf_rtrack@elementMetadata$transcript_id
+    gtf.rtrack@elementMetadata$exon_id <- gtf.rtrack@elementMetadata$transcript_id
 
     #make exon names
     n <- 1
     while(any(is.na(gtf.rtrack@elementMetadata$exon_number))){
-      transcript_ids <- unique(gtf_rtrack@elementMetadata$transcript_id)
+      transcript_ids <- unique(gtf.rtrack@elementMetadata$transcript_id)
       m <- match(transcript_ids,
                  gtf.rtrack@elementMetadata$transcript_id[which(
                    is.na(gtf.rtrack@elementMetadata$exon_number))])
@@ -48,24 +47,8 @@ gtfToExons <- function(gtf, outputFile=NA){
     }
   }
 
-  exons <- cbind(as.character(GenomicRanges::seqnames(gtf_rtrack)),
-                 gtf.rtrack@elementMetadata[,c(2)],
-                 as.data.frame(GenomicRanges::ranges(gtf_rtrack))[,-3],
-                 as.character(GenomicRanges::strand(gtf_rtrack)),
-                 gtf.rtrack@elementMetadata[,c("gene_id","gene_type",
-                                               "transcript_id","transcript_type",
-                                               "exon_number","exon_id")])
-
-  colnames(exons) <- c("chromosome","type","start","end",
-                       "strand","gene_id","gene_type",
-                       "transcript_id","transcript_type",
-                       "exon_number","exon_id")
-
-  if(!is.na(outputFile)){
-    utils::write.table(exons, file=outputFile,
-                       quote=FALSE, col.names =FALSE, 
-                       row.names = FALSE, sep ="\t")
-  }
-
+  exons <- gtf.rtrack
+  exons@elementMetadata[c(1,3,4,7:10,13:19)] <- NULL
+  
   return(exons)
 }
