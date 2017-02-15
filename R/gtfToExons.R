8235a7cf008f8b3a3d3e50e1fc0d219d1d503512
betsig <b.signal@garvan.org.au>
2017-02-15 11:16:02 +1100

    Changed _underscores_ to CamelCase where appropriate

--- R/gtfToExons.R
+++ R/gtfToExons.R
@@ -2,8 +2,7 @@
 #'
 #' Converts a GTF annotation to exon locations
 #' @param gtf file containing the gtf annotation.
-#' @param output_file name of the file to write the exon annotation to.
-#' If not specified, will write to the same location as the gtf file.
+#' @param outputFile (optional) name of the file to write the exon annotation to.
 #' @return exon annotation data.frame
 #' @export
 #' @import stringr
@@ -18,42 +17,42 @@
 #' exons <- gtfToExons(small_gtf)
 #' @author Beth Signal
 
-gtfToExons <- function(gtf, output_file=NA){
+gtfToExons <- function(gtf, outputFile=NA){
 
-  gtf_rtrack <- rtracklayer::import(gtf)
+  gtf.rtrack <- rtracklayer::import(gtf)
 
   #keep exon annotations only
-  gtf_rtrack <- gtf_rtrack[gtf_rtrack@elementMetadata$type=="exon"]
+  gtf.rtrack <- gtf.rtrack[gtf.rtrack@elementMetadata$type=="exon"]
 
   #change "biotype" to "type" in GTFs from Ensembl
-  if(any(grepl("biotype", names(gtf_rtrack@elementMetadata)))){
-    names(gtf_rtrack@elementMetadata) <- gsub("biotype",
+  if(any(grepl("biotype", names(gtf.rtrack@elementMetadata)))){
+    names(gtf.rtrack@elementMetadata) <- gsub("biotype",
                                               "type",
-                                              names(gtf_rtrack@elementMetadata))
+                                              names(gtf.rtrack@elementMetadata))
   }
 
-  if(all(!grepl("exon", names(gtf_rtrack@elementMetadata)))){
-    gtf_rtrack@elementMetadata$exon_number <- NA
-    gtf_rtrack@elementMetadata$exon_id <- gtf_rtrack@elementMetadata$transcript_id
+  if(all(!grepl("exon", names(gtf.rtrack@elementMetadata)))){
+    gtf.rtrack@elementMetadata$exon_number <- NA
+    gtf.rtrack@elementMetadata$exon_id <- gtf_rtrack@elementMetadata$transcript_id
 
     #make exon names
     n <- 1
-    while(any(is.na(gtf_rtrack@elementMetadata$exon_number))){
+    while(any(is.na(gtf.rtrack@elementMetadata$exon_number))){
       transcript_ids <- unique(gtf_rtrack@elementMetadata$transcript_id)
       m <- match(transcript_ids,
-                 gtf_rtrack@elementMetadata$transcript_id[which(
-                   is.na(gtf_rtrack@elementMetadata$exon_number))])
-      gtf_rtrack@elementMetadata$exon_number[which(
-        is.na(gtf_rtrack@elementMetadata$exon_number))[m]] <- n
+                 gtf.rtrack@elementMetadata$transcript_id[which(
+                   is.na(gtf.rtrack@elementMetadata$exon_number))])
+      gtf.rtrack@elementMetadata$exon_number[which(
+        is.na(gtf.rtrack@elementMetadata$exon_number))[m]] <- n
       n <- n+1
     }
   }
 
   exons <- cbind(as.character(GenomicRanges::seqnames(gtf_rtrack)),
-                 gtf_rtrack@elementMetadata[,c(2)],
+                 gtf.rtrack@elementMetadata[,c(2)],
                  as.data.frame(GenomicRanges::ranges(gtf_rtrack))[,-3],
                  as.character(GenomicRanges::strand(gtf_rtrack)),
-                 gtf_rtrack@elementMetadata[,c("gene_id","gene_type",
+                 gtf.rtrack@elementMetadata[,c("gene_id","gene_type",
                                                "transcript_id","transcript_type",
                                                "exon_number","exon_id")])
 
@@ -62,9 +61,10 @@ gtfToExons <- function(gtf, output_file=NA){
                        "transcript_id","transcript_type",
                        "exon_number","exon_id")
 
-  if(!is.na(output_file)){
-    utils::write.table(exons, file=output_file,
-                       quote=FALSE, col.names =FALSE, row.names = FALSE, sep ="\t")
+  if(!is.na(outputFile)){
+    utils::write.table(exons, file=outputFile,
+                       quote=FALSE, col.names =FALSE, 
+                       row.names = FALSE, sep ="\t")
   }
 
   return(exons)
