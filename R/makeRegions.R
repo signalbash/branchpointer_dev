210374f827caa1cd89b8572c3c0466222f2676c0
betsig <b.signal@garvan.org.au>
2017-05-24 07:44:53 +1000

    allow multiple query region processing & speed improvements

--- R/makeRegions.R
+++ R/makeRegions.R
@@ -2,10 +2,15 @@
 #'
 #' Genrate branchpoint window regions corresponding to annotated exon(s) within a
 #' queried gene, transcript or exon id
-#' @param id identifier for the query gene/transcript/exon id
+#' @param id identifier(s) for the query gene/transcript/exon id
 #' @param idType type of id to match in the exon annotation file (\code{"gene_id"},
 #' \code{"transcript_id"}, or \code{"exon_id"})
-#' @param exons data.frame containing exon co-ordinates.
+#' @param exons GRanges containing exon co-ordinates.
+#' @param forceClosestExon Force branchpointer to find the closest exon and not the
+#' exon annotated as 5' to the query
+#' @param useParallel use parallelisation to speed up code? - passed to getQueryLoc
+#' (reccomended for > 500 query entries)
+#' @param cores number of cores to use in parallelisation (default = \code{1})
 #' Should be produced by gtfToExons()
 #' @return Granges with formatted query
 #' @export
@@ -18,7 +23,8 @@
 #' windowquery <- makeRegions("ENSE00003518965", "exon_id", exons)
 #' @author Beth Signal
 
-makeRegions <- function(id, idType, exons) {
+makeRegions <- function(id, idType, exons,forceClosestExon = FALSE, 
+                        useParallel=FALSE, cores=1) {
 
   validTypes <- c("gene_id", "transcript_id","exon_id")
 
@@ -32,7 +38,8 @@ makeRegions <- function(id, idType, exons) {
 
   if (!noType) {
     x <- which(colnames(mcols(exons)) == idType)
-    y <- grep(id, mcols(exons)[,x])
+    y <- which(!is.na(match(mcols(exons)[,x], id)))
+    #y <- grep(id[1], mcols(exons)[,x])
   }else{
     y <- NA
   }
@@ -41,55 +48,95 @@ makeRegions <- function(id, idType, exons) {
   if (all(is.na(y)) | noType) {
     idType <- validTypes[1]
     x <- which(colnames(mcols(exons)) == idType)
-    y <- grep(id, mcols(exons)[,x])
+    y <- which(!is.na(match(mcols(exons)[,x], id)))
+    #y <- grep(id[1], mcols(exons)[,x])
 
     if (length(y) == 0) {
       idType <- validTypes[2]
       x <- which(colnames(mcols(exons)) == idType)
-      y <- grep(id, mcols(exons)[,x])
+      y <- which(!is.na(match(mcols(exons)[,x], id)))
+      #y <- grep(id[1], mcols(exons)[,x])
     }
 
     if (length(y) == 0) {
       idType <- validTypes[3]
       x <- which(colnames(mcols(exons)) == idType)
-      y <- y <- grep(id, mcols(exons)[,x])
+      y <- which(!is.na(match(mcols(exons)[,x], id)))
+      #y <- grep(id[1], mcols(exons)[,x])
     }
 
     if (length(y) == 0) {
-      stop(paste0("cannot find ", id," in the exon annotation"))
+      stop(paste0("cannot find ", id[1]," in the exon annotation"))
     }
 
   }
 
   #use a subset of the exon annotation for faster processing
-  if (idType != "gene_id") {
-    gene_id <- exons$gene_id[y[1]]
-    y2 <- which(!is.na(match(exons$gene_id,gene_id)))
-  }else{
-    y2 <- y
-  }
-
-  exons.subset <- exons[y]
-
-  #by definition first exons shouldn't have branchpoints
-  keep <- which(exons.subset$exon_number > 1)
-
-  if (as.logical(strand(exons.subset)[1] == "+")) {
-    windowStarts <- (start(ranges(exons.subset)) - 50)[keep]
-  }else{
-    windowStarts <- (end(ranges(exons.subset)) + 10)[keep]
-  }
-
-  window <- exons.subset[keep]
-
-  # giving errors trying to set as start(ranges(window))
-  window@ranges@start <- as.integer(windowStarts)
-  width(ranges(window)) <- 41
   
-  window@elementMetadata$id <- window@elementMetadata$exon_id
+  gene_id <- unique(mcols(exons)$gene_id[y])
+  subsetInd <- which(mcols(exons)$gene_id %in% gene_id)
+  exons.subset <- exons[subsetInd]
+  
+  keep <- which(mcols(exons[y])$exon_number > 1)
+  window <- exons[y][keep]
+  
+  # manually generate windows
+
+  # make window starts & ends
+  window.ends <- start(window) - 18
+  window.starts <- window.ends - 26
+    
+  negStrandIndex <- which(as.logical(strand(window) == "-"))
+    
+  window.starts[negStrandIndex] <- 
+    end(window)[negStrandIndex] + 18
+  window.ends[negStrandIndex] <- 
+    window.starts[negStrandIndex] + 26
+    
+  window.IRanges <- IRanges(start=window.starts, end=window.ends)
+  # replace ranges
+  ranges(window) <- window.IRanges
+    
+  # skips getQueryLoc() -- which can be time limiting step
+  if(forceClosestExon == FALSE){
+    
+    # to_3prime will be 18 for all windows
+    mcols(window)$to_3prime <- 18
+    
+    # get 5' exon
+    # Note that this is not neccesarily the 'closest' exon
+    # closest can be enforced with forceClosestExon = TRUE
+    transcriptExonNum.5prime <- paste0(mcols(window)$transcript_id,"_",
+                                       as.numeric(mcols(window)$exon_number) -1)
+    transcriptExonNum <- paste0(mcols(exons.subset)$transcript_id,"_",
+                                as.numeric(mcols(exons.subset)$exon_number))
+    
+    m <- match(transcriptExonNum.5prime, transcriptExonNum)
+    to_5prime <- end(window) - end(exons.subset[m])
+    to_5prime[negStrandIndex] <- 
+      (start(exons.subset[m]) - start(window))[negStrandIndex]
+    mcols(window)$to_5prime <- to_5prime
+    mcols(window)$same_gene <- TRUE
+    mcols(window)$exon_3prime <- mcols(window)$exon_id
+    mcols(window)$exon_5prime <- mcols(exons.subset[m])$exon_id
+  }else{
+      window <- getQueryLoc(window,queryType = "region",exons = exons.subset, 
+                  useParallel = useParallel, cores=cores)
+  }
+    
+  mcols(window)$id <- mcols(window)$exon_id
+  
+  windowNames <- paste0(seqnames(window), "_", 
+                        start(window),"_", 
+                        end(window), "_", 
+                        strand(window))
+  rm <- which(duplicated(windowNames))
+  if(length(rm) > 0){
+    window <- window[-rm]
+  }
 
   if(length(window) > 0){
-    return(getQueryLoc(window,queryType = "region",exons = exons[y2]))
+    return(window)
   }
 
 }
